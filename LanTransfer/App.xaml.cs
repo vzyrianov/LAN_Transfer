d1a8a3f3f950e359202a7431020d78dad1a8043b
Vlas Zyrianov <vzyriano@kent.edu>
2019-08-30 14:10:38 -0400

    Add sln and start work

---
+++ LanTransfer/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace LanTransfer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Window window;
+
+            window = new Server("hello");
+            /*
+            if (e.Args.Length == 1)
+            {
+                MessageBox.Show("Now opening file: \n\n" + e.Args[0]);
+                window = new Server(e.Args[0]);
+            }
+            else
+            {
+                window = new Client();
+            }*/
+
+            window.Show();
+        }
+    }
+}
