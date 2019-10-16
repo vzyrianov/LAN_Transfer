9884f85187ea3a919c3f63a02f9ddddf12e5c60f
vzyrianov <vzyriano@kent.edu>
2019-10-15 21:42:26 -0400

    Continue work on prototype

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ from flask import Flask, request
 from flask import send_file,request
 from flask import render_template
 from flask import make_response
-from flask import redirect
-from flask import jsonify
 
+from os import listdir
+from os.path import isfile, join
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -13,17 +13,16 @@ password = ""
 
 @app.route('/', methods = ["GET"])
 def index():
-   return ""
-    #return render_template("index.html") 
+
+   files = [f for f in listdir('.') if isfile(join('.',f))]
+   
+   return render_template("index.html", files = files) 
 
 @app.route('/file', methods = ["GET", "POST"])
 def file():
    #filename = request.form['name']
    filename = request.args.get('filename')
-   if(filename == 'test.txt'): 
-      return send_file(filename)
-   else:
-      return ""
+   return send_file(filename)
 
 
 if __name__ == '__main__':
